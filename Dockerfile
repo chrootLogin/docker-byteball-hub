ef769e39e3f071b18287933793244d183f1aecfc
Simon Erhardt <me@rootlogin.ch>
2017-08-21 20:17:01 +0200

    added env variables

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ MAINTAINER Simon Erhardt <me+docker@rootlogin.ch>
 ARG UID=1555
 ARG GID=1555
 
+ENV PORT=6611 \
+  MYURL=""
+
 COPY root /
 
 RUN set -ex \
@@ -16,8 +19,7 @@ RUN set -ex \
   # Add user
   && addgroup -g ${GID} byteball \
   && adduser -u ${UID} -h /opt/byteball -H -G byteball -s /sbin/nologin -D byteball \
-  && mkdir -p /opt/byteball \
-  && chown byteball:byteball /opt/byteball \
+  && chown -R byteball:byteball /opt/byteball \
 
   # Install byteball
   && git clone --depth 1 https://github.com/byteball/byteball-hub.git /opt/byteball-hub \
@@ -26,6 +28,8 @@ RUN set -ex \
 
   # Post-installation steps
   && chmod +x /usr/local/bin/run.sh
+
+EXPOSE 6611
 
 ENTRYPOINT ["/sbin/tini", "--"]
 CMD ["/usr/local/bin/run.sh"]
---
+++ root/opt/byteball/conf.js
@@ -0,0 +1,52 @@
+/*jslint node: true */
+"use strict";
+
+exports.clientName = 'byteball';
+exports.minClientVersion = '1.10.0';
+
+// https://console.developers.google.com
+exports.pushApiProjectNumber = 0;
+exports.pushApiKey = '';
+
+if(process.env.PORT == "") {
+  process.exit(1);
+}
+exports.port = process.env.PORT;
+
+if(process.env.MYURL == "") {
+  process.exit(1);
+}
+exports.myUrl = 'wss://' + process.env.MYURL + '/bb';
+exports.bServeAsHub = true;
+exports.bSaveJointJson = true;
+exports.bLight = false;
+
+// this is used by wallet vendor only, to redirect bug reports to developers' email
+exports.bug_sink_email = 'admin@example.org';
+exports.bugs_from_email = 'bugs@example.org';
+
+exports.HEARTBEAT_TIMEOUT = 300*1000;
+
+exports.storage = 'sqlite';
+
+
+exports.initial_witnesses = [
+	'BVVJ2K7ENPZZ3VYZFWQWK7ISPCATFIW3',
+	'DJMMI5JYA5BWQYSXDPRZJVLW3UGL3GJS',
+	'FOPUBEUPBC6YLIQDLKL6EW775BMV7YOH',
+	'GFK3RDAPQLLNCMQEVGGD2KCPZTLSG3HN',
+	'H5EZTQE7ABFH27AUDTQFMZIALANK6RBG',
+	'I2ADHGP4HL6J37NQAD73J7E5SKFIXJOT',
+	'JEDZYC2HMGDBIDQKG3XSTXUSHMCBK725',
+	'JPQKPRI5FMTQRJF4ZZMYZYDQVRD55OTC',
+	'OYW2XTDKSNKGSEZ27LMGNOPJSYIXHBHC',
+	'S7N5FE42F6ONPNDQLCF64E2MGFYKQR2I',
+	'TKT4UESIKTTRALRRLWS4SENSTJX6ODCW',
+	'UENJPVZ7HVHM6QGVGT6MWOJGGRTUTJXQ'
+];
+
+exports.initial_peers = [
+	'wss://byteball.org/bb'
+];
+
+console.log('finished hub conf');
